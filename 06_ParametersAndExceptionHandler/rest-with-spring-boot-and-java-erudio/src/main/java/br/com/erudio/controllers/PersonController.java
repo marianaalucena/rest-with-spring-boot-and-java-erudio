b9d1e90a5e706d8b564b1ca305f0f9c8e4e9129a
DIR\mariana.a.lucena <mariana.a.lucena@accenture.com>
2024-10-29 18:30:00 -0300

    Seção 11: Migrations com FlyWay

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.erudio.data.vo.v2.PersonVoV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired //o spring cuida da instaciacao
---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2_Populate_Table_Person2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(3,'Rio de Janeiro - Brasil','Pedro','Male','Nascimento'),
+(4,'Rio de Janeiro - Brasil','Pedro','Male','Nascimento'),
+(5,'Campina Grande - Brasil','Mariana','Male','Araujo'),
+(6,'Campina Grande - Brasil','Silvana','Male','Araujo');
