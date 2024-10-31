b951ed3da3ffe3087813f6672d29bf6b85107129
DIR\mariana.a.lucena <mariana.a.lucena@accenture.com>
2024-10-31 17:07:28 -0300

    Seção 12: Custom JSON Serialization

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVo.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVo.java
@@ -3,12 +3,20 @@ package br.com.erudio.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender" })
 public class PersonVo implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
 	private String gender;
--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2_Populate_Table_Person2.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
-(3,'Rio de Janeiro - Brasil','Pedro','Male','Nascimento'),
-(4,'Rio de Janeiro - Brasil','Pedro','Male','Nascimento'),
-(5,'Campina Grande - Brasil','Mariana','Male','Araujo'),
-(6,'Campina Grande - Brasil','Silvana','Male','Araujo');
